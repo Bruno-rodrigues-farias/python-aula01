d0ad8daf252a15796cf57ef7303ee7a154067511
Bruno Rodrigues <140970160+BrunoRodriguesDEV@users.noreply.github.com>
2023-09-10 14:46:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+
+#Atividade 11
+
+# salario_colaborador = int(input("Digite o sálario do colaborador: "))
+
+# if salario_colaborador < 280.00:
+#    aumento = (20*100)/salario_colaborador
+#    novo_salario = salario_colaborador + aumento
+   
+#    print(f"O salário antes do reajuste: {salario_colaborador}")
+#    print(f"O percentual de aumento aplicado: 20%")
+#    print(f"O valor do aumento: {aumento}")
+#    print(f"O novo salário, após o aumento: {novo_salario}")
+
+
+# elif salario_colaborador == 280 or salario_colaborador < 700.00:
+#      aumento = (15*100)/salario_colaborador 
+#      novo_salario = salario_colaborador + aumento
+#      print(f"O salário antes do reajuste: {salario_colaborador}")
+#      print(f"O percentual de aumento aplicado: 15%")
+#      print(f"O valor do aumento: {aumento}")
+#      print(f"O novo salário, após o aumento: {novo_salario}")
+      
+
+# elif salario_colaborador == 700.00 or salario_colaborador < 1500.00:
+#     aumento = (10*100)/salario_colaborador 
+#     novo_salario = salario_colaborador + aumento
+#     print(f"O salário antes do reajuste: {salario_colaborador}")
+#     print(f"O percentual de aumento aplicado: 10%")
+#     print(f"O valor do aumento: {aumento}")
+#     print(f"O novo salário, após o aumento: {novo_salario}")
+        
+
+# elif salario_colaborador >= 1500.00:
+#     aumento = (5*100)/salario_colaborador 
+#     novo_salario = salario_colaborador + aumento
+#     print(f"O salário antes do reajuste: {salario_colaborador}")
+#     print(f"O percentual de aumento aplicado: 5%")
+#     print(f"O valor do aumento: {aumento}")
+#     print(f"O novo salário, após o aumento: {novo_salario}")
+
+# n = int(input("Digite um numero inteiro e positivo: "))
+
+# s = 0
+
+# texto = ""
+
+# if n > 0:
+#     for i in range(1, n+1):
+#         div = 1 / i
+
+#         s += div
+
+#         if i == 1:
+#             texto += "1"
+#         else:
+#             texto += f" + 1/{i}"
+
+    
+#     print(f"Valor final é de: S = {texto} = {s}")
+
+# else:
+#     print("Numero indicado é menor ou igual a zero, tente novamente.")
+
+# n = int(input("Digite um numero inteiro e positivo: "))
+# s = 0
+
+# print("S =", end="")
+# if n > 0:
+#     for i in range(1, n+1):
+#         div = 1 / i
+
+#         s += div
+
+#         if i == 1:
+#             print(" 1", end="")
+#         else:
+#             print(f" + 1/{i}", end="")
+
+    
+#     print(f" = {s}")
+
+# else:
+#     print("Numero indicado é menor ou igual a zero, tente novamente.")
